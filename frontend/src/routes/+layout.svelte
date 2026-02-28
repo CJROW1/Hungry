eaf0c9f8d19e536212f0f2fe31a7822ea1b008d8
Rylee <230508050+ryle4e@users.noreply.github.com>
2026-02-28 05:43:11 -0800

    fixed mismatching fonts

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,6 +6,12 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@600;700;800&display=swap"
+		rel="stylesheet"
+	/>
 </svelte:head>
 
 {@render children()}
