6be9970d9de5a081313759b1350a629d1ca2e574
katpium <uwuing88@gmail.com>
2026-02-28 03:09:25 -0800

    lam file

--- frontend/src/routes/lam.svelte
+++ frontend/src/routes/lam.svelte
@@ -1,5 +1,33 @@
-<style>
-
-</style>
-
 <p>Lam's stuff here</p>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<style>
+			* {
+				box-sizing: border-box;
+			}
+			.header {
+				background-color: #292929;
+				padding: 10px;
+				display: flex;
+				justify-content: flex-end; /* moves to the right */
+				gap: 10px;
+			}
+
+			.
+			
+			@media screen and (max-width: 800px) {
+  				.header {
+ 				    width: 100%; /* The width is 100%, when the viewport is 800px or smaller */
+  				}
+			}
+		</style>
+	</head>
+    <body>
+        <div class ="header"></div>
+    </body>
+
+</html>
+
