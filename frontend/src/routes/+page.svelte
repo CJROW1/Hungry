2e61cba865e6fd84aa1241279d7be08b9f0a0365
Tung Do <134229448+tungdo8842@users.noreply.github.com>
2026-02-27 21:28:29 -0800

    Add first api call

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -3,12 +3,22 @@
 
     let number = $state(1);
     let test_call = $state("");
+    let user = $state("")
 
     onMount(async () =>
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon")
+        fetch("http://localhost:8000/greeting", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: "Jesse",
+                age: 99,
+            })
+        })
         .then(response => response.json())
         .then(data => {
-            test_call = data.drinks[0].strDrink;
+            user = data.message;
             console.log("state below this");
             console.log($state.snapshot(test_call));
         })
@@ -18,4 +28,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <p>Testing API call {number}</p>
-<p>Testing API call {test_call}</p>
+<p>Testing API call: {user}</p>
