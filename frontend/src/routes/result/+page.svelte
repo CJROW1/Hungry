abebcf6529cfed0c37eb96e3860a7578b9a73386
Rylee <230508050+ryle4e@users.noreply.github.com>
2026-02-28 13:43:26 -0800

    gwg

--- frontend/src/routes/result/+page.svelte
+++ frontend/src/routes/result/+page.svelte
@@ -1,12 +1,5 @@
 <script>
-<<<<<<< HEAD
-import Result from "$lib/components/result.svelte";
-</script>
-
-<Result />
-=======
     import Result from "$lib/components/result.svelte";
 </script>
 
 <Result />
->>>>>>> 52618d0857bdd6b3afda1c5a53146f4027f326e7
